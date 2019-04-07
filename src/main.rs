5953e2ca5c6113bd510d686a6e9f0f5ea5cb7ea9
Haoran Wang <waharaxn@gmail.com>
2019-04-07 01:14:37 -0700

    Add cfg to conditional build some parts of the program for debug purpose

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,28 @@ fn main() -> Result<(), String> {
     let c_src_name = &args[1];
     let s_src_name = &args[2];
     let contents = fs::read_to_string(c_src_name).expect("Can't read file");
-    // println!("--------------------------------");
-    // println!("SOURCE_FILE: [{}]", c_src_name);
-    // println!("--------------------------------");
-    // println!("{}", contents);
+    if cfg!(feature = "source") {
+        println!("--------------------------------");
+        println!("SOURCE_FILE: [{}]", c_src_name);
+        println!("--------------------------------");
+        println!("{}", contents);
+    }
 
     let token_list = r#try!(lexer::lex(&contents));
-    // println!("--------------------------------");
-    // println!("Token List : ");
-    // println!("{:?}", token_list);
-    // println!("number of tokens: {}", token_list.len());
+    if cfg!(feature = "token") {
+        println!("tokens: {:?}\n", token_list);
+        println!("number of tokens: {}", token_list.len());
+    }
 
     let root_node = r#try!(parser::parse_prog(&contents, c_src_name));
-    // println!("--------------------------------");
-    // println!("AST nodes:\n{}", parser::print(&root_node, 0));
+    if cfg!(feature = "ast") {
+        println!("\nAST nodes:\n{}\n", parser::print(&root_node, 0));
+    }
 
     let s_contents = gen::gen_prog(&root_node);
-    // println!("--------------------------------");
-    // println!("AS FILE:\n{}", s_contents);
+    if cfg!(feature = "as") {
+        println!("\nAS FILE:\n{}", s_contents);
+    }
 
     fs::write(s_src_name, s_contents).expect("Can't write assembly code");
     Ok(())
