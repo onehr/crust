0f2f4495c714ef15b30ff31e94a84a7c307a278a
Haoran Wang <waharaxn@gmail.com>
2019-04-14 22:16:29 -0700

    add some trigraph tests

---
+++ test/valid/cpp/tri_1.c
@@ -0,0 +1,4 @@
+
+int main(void) ??<
+        printf("what??!\n");
+??>
---
+++ test/valid/cpp/tri_3.c
@@ -0,0 +1,15 @@
+
+int a??(100??);
+
+int main(void) ??<
+        for (int i = 0; i < 100; i++) ??<
+                a??(i??) = ??-i ??! 1 ^ 100;
+        ??>
+        int b = 10;
+        for (int i = 0; i < 100; i++) ??<
+                printf("%d ", a??(i??));
+        ??>
+        printf("??/n");
+        printf("%d", b);
+        return 0;
+??>
