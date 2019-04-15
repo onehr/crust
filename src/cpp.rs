d2ae56b327a3dccc73914c15e92aa9663cab97b5
Haoran Wang <waharaxn@gmail.com>
2019-04-14 23:45:10 -0700

    Add comment remove

--- src/cpp.rs
+++ src/cpp.rs
@@ -16,6 +16,8 @@
 // cpp.rs: Simple c preprocessor
 // -----------------------------------------------------------------------------
 
+use std::process::id;
+
 fn trigraph_processor(input: String) -> Result<String, String> {
     // Trigraph:       ??(  ??)  ??<  ??>  ??=  ??/  ??'  ??!  ??-
     // Replacement:      [    ]    {    }    #    \    ^    |    ~
@@ -78,9 +80,111 @@ fn trigraph_processor(input: String) -> Result<String, String> {
     return Ok(res);
 }
 
+fn line_concat(input: String) -> Result<String, String> {
+    let mut res = String::new();
+    let mut it = input.chars().peekable();
+    while let Some(&c) = it.peek() {
+        if c == '\\' {
+            it.next();
+            if let Some(&nc) = it.peek() {
+                match nc {
+                    '\n' => {
+                        // remove this `\` and `\n`
+                        // so nothing here, just skip
+                    }
+                    _ => {
+                        res.push(c);
+                        res.push(nc);
+                    }
+                }
+            } else {
+                // no other characters, at the end of file
+                // Should give warning, but continue compile.
+                // cause crust now has no warning option, so just remove this `\`
+                break;
+            }
+        } else {
+            res.push(c);
+        }
+        it.next();
+    }
+
+    return Ok(res);
+}
+
+fn in_comment(single: bool, multi: bool) -> bool {
+    return single || multi;
+}
+
+fn remove_comment(input: String) -> Result<String, String> {
+    let mut res = String::new();
+
+    let mut idx = 0;
+    let mut single_line_in_comment = false;
+    let mut multi_line_in_comment = false;
+    while idx < input.len() {
+        if idx == input.len() - 1 {
+            if single_line_in_comment || multi_line_in_comment {
+                // skip
+                break;
+            } else {
+                res.push(char::from(input.as_bytes()[idx]));
+                break;
+            }
+        }
+        let b1 = char::from(input.as_bytes()[idx]);
+        let b2 = char::from(input.as_bytes()[idx+1]);
+
+        let mut combine = String::new();
+        combine.push(b1);
+        combine.push(b2);
+        match combine.as_ref() {
+            "//" => {
+                if !in_comment(single_line_in_comment, multi_line_in_comment) {
+                    single_line_in_comment = true;
+                }
+                idx = idx + 2;
+            }
+            "/*" => {
+                if !in_comment(single_line_in_comment, multi_line_in_comment) {
+                    multi_line_in_comment = true;
+                }
+                idx = idx + 2;
+            }
+            "*/" => {
+                if multi_line_in_comment {
+                    multi_line_in_comment = false;
+                    idx = idx + 2;
+                } else {
+                    res.push(b1);
+                    idx = idx + 1;
+                }
+            }
+            _ => {
+                if b1 == '\n' && single_line_in_comment {
+                    single_line_in_comment = false;
+                    idx = idx + 1;
+                } else {
+                    if in_comment(single_line_in_comment, multi_line_in_comment) {
+                        idx = idx + 1;
+                    } else {
+                        res.push(b1);
+                        idx = idx + 1;
+                    }
+                }
+            }
+        }
+    }
+    return Ok(res);
+}
+
 pub fn cpp_driver(input: String) -> Result<String, String> {
     // first translate trigraph into chars
     let after_cpp_str = trigraph_processor(input)?;
+    // concatenate lines
+    let after_cpp_str = line_concat(after_cpp_str)?;
+    // remove comment
+    let after_cpp_str = remove_comment(after_cpp_str)?;
 
     Ok(after_cpp_str)
 }
---
+++ test/valid/cpp/comment_1.c
@@ -0,0 +1,23 @@
+int a[100];
+int b[100][100][100];
+int c[100];
+int main() {
+        a[0] = 1;
+        a[1] = 2;
+        a[3] = a[0] + a[1];
+        // adfsaf  \
+        dafdaffafsaf\
+        adsfafafas\
+
+        /* this is a comment for the preprocessor to handle */
+        /*
+          try multiline
+         */
+        // /* try some thing
+        /*
+          printf("%d\ n", a[0]);
+// */
+
+        printf("%d\n", a[1]);
+        printf("%d\n", a[2]);
+}
